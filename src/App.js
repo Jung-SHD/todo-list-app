fdbe02ce038e91b341d3f7fd89c118cf5dba1e1b
Jung <jundev2021@gmail.com>
2024-05-21 22:45:03 +0900

    [2/n] Add TodoItemList react component to display todo items: Make TodoItemList component to take an array of todo items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
-import './App.css';
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import "./App.css";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
@@ -10,27 +9,38 @@ const TodoItemInputField = (props) => {
     props.onSubmit(input);
     setInput("");
   };
-    
+
   return (
     <div>
       <TextField
         id="todo-item-input"
         label="Todo Item"
         variant="outlined"
-        onChange={(e) => setInput(e.target.value)} value={input}
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
       />
-      <Button variant="outlined" onClick={onSubmit}>Submit</Button>
+      <Button variant="outlined" onClick={onSubmit}>
+        Submit
+      </Button>
     </div>
   );
-}
-const TodoItemList = () =>{
-  return(<div></div>);
-}
+};
+
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>;
+  });
+  return (
+    <div>
+      <ul>{todoList}</ul>
+    </div>
+  );
+};
 function App() {
   return (
     <div className="App">
-      <TodoItemInputField onSubmit={() => {}}/>
-      <TodoItemList/>
+      <TodoItemInputField onSubmit={() => {}} />
+      <TodoItemList todoItemList={[]} />
     </div>
   );
 }
